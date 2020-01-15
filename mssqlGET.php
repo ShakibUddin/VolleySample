ba39e481dd7eee43f839b9aa9596a18264c811bc
Md Shakib Uddin Bhuiyan <shakibuddinbhuiyan@gmail.com>
2020-01-15 17:38:42 +0600

    ok

---
+++ mssqlGET.php
@@ -0,0 +1,24 @@
+<?php
+$serverName = "DESKTOP-98S3HTF\SHAKIBUDDINSQL";
+$connectionInfo = array( "Database"=>"student", "UID"=>"", "PWD"=>"");
+$conn = sqlsrv_connect( $serverName, $connectionInfo );
+if( $conn === false ) {
+    die( print_r( sqlsrv_errors(), true));
+    echo json_encode(array('response'=>'Failed to connect to Mssql'));
+}
+
+$sql = "SELECT * FROM data";
+$retrieved_data = array();
+$stmt = sqlsrv_query( $conn, $sql );
+if( $stmt === false) {
+    die( print_r( sqlsrv_errors(), true) );
+    echo json_encode(array('response'=>'Failed to connect to Mssql'));
+}
+
+while( $row = sqlsrv_fetch_array( $stmt, SQLSRV_FETCH_ASSOC) ) {
+      $data=['name'=>$row["name"],'pass'=>$row["pass"]];
+      array_push($retrieved_data, $data);
+}
+echo json_encode(['retrieved_data'=>$retrieved_data]);
+sqlsrv_free_stmt( $stmt);
+?>
